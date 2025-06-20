8bcec2114da9be425070eec76e6c00f4aa552c21
Shalenyi Matvii <156939063+SMatvii@users.noreply.github.com>
2025-06-20 21:42:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from flask import Flask
+from backend.api import app as api_app
+
+app = Flask(__name__, static_folder="frontend/static", template_folder="frontend/templates")
+app.register_blueprint(api_app, url_prefix="/api")  # якщо api з blueprints, інакше просто імпорт
